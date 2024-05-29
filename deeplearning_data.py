606a5de57376960c5d53686e525ce091cdf217fa
sheepincastle <diddlstjd70@gmail.com>
2024-05-30 00:48:19 +0900

    v2

--- deeplearning_data.py
+++ deeplearning_data.py
@@ -2,8 +2,15 @@ import dlib
 import cv2
 import glob
 import math
-import csv
 import pandas
+
+'''
+dlib을 이용해 얼굴 이미지 분석
+opencv(=cv2)를 이용해 이미지를 엶
+glob을 이용해 대량의 이미지 및 자료 한번에 엶
+math를 이용해 사칙연산 이외의 수학 연산 수행
+pandas를 이용해 행렬 생성 및 관리
+'''
 
 #dlib에 있는 얼굴 인식 AI 모델 가져옴
 detector = dlib.get_frontal_face_detector()
@@ -19,22 +26,22 @@ def find_eyes(img):
     result = []
     
     if len(dets) == 0:
-        #얼굴을 찾지 못했을 때 반환할 값: 0
+        #얼굴을 찾지 못했을 때 빈 리스트를 반환하여 저장할 값에서 배제
         return result
     
-    #튜플의 형태로 dets 변환, dets의 개수만큼 반복
+    #dets의 개수만큼 반복문 반복
     #k는 사람 얼굴마다 주어지는 번호
     #d는 얼굴을 담은 박스
     for k, d in enumerate(dets):
         #사람의 얼굴을 나타내는 68개의 랜드마크(점) 저장
         shape = sp(img, d)
         
-        #눈 랜드마크의 좌표 저장
-        for i in range(6):
+        #눈을 나타내는 랜드마크의 좌표 저장
+        for i in range(6):#왼쪽 눈
             x = shape.part(i+36).x
             y = shape.part(i+36).y
             left_landmarks.append((x, y))
-        for i in range(6):
+        for i in range(6):#오른쪽 눈
             x = shape.part(i+42).x
             y = shape.part(i+42).y
             right_landmarks.append((x, y))
@@ -55,16 +62,16 @@ def find_eyes(img):
 
 #이미지 불러오기
 #0은 닫은 눈, 1은 열린 눈
-img_list_0 = glob.glob('material data/ClosedFace/*.jpg')
-img_list_1 = glob.glob('material data/OpenFace/*.jpg')
+img_list_0 = glob.glob('material data/ClosedFace/*.jpg')#닫은 눈 이미지들
+img_list_1 = glob.glob('material data/OpenFace/*.jpg')#열린 눈 이미지들
 print('img0: ', len(img_list_0))
 print('img1: ', len(img_list_1))
 
-#학습할 데이터, 정답을 저장할 행렬 생성
+#학습할 데이터와 정답을 저장할 행렬 생성
 eye_data = pandas.DataFrame(columns=['distance1', 'distance2', 'distance3', 'distance4', 'distance5', 'distance6', 'value'], index=[], dtype=float)
 
 #학습할 데이터 및 정답 저장
-#find_eyes함수의 결과로 나온 리스트의 항목이 6개 미만이라면, 즉 입력값의 일부가 없다면 continue를 통해 데이터를 저장하지 않고 다음 사진으로 넘어감
+#find_eyes함수의 결과로 나온 리스트의 항목이 7 미만이라면, 즉 값의 일부가 없다면 continue를 통해 데이터를 저장하지 않고 다음 사진으로 넘어감
 for i in range(len(img_list_0)):
     a = find_eyes(cv2.imread(img_list_0[i]))
     a.append(0)
@@ -80,7 +87,7 @@ for i in range(len(img_list_1)):
     
 print(eye_data)
 
-#결과값을 저장한 2차원 배열과 정답을 저장한 배열을 pickle파일 형태로 저장
+#값을 저장한 행렬을 pickle의 형태로 데이터 저장
 eye_data.to_pickle('eye_data.pkl')
     
 print('fin')
--- projectY.py
+++ projectY.py
@@ -2,12 +2,31 @@ import dlib
 import cv2
 import numpy as np
 import math
+import keras
+import time
+import pyfirmata
+#time을 이용해 시간 측정
+#pyfirmata 이용해 아두이노 조작
+#아두이노 포트, 부저 위치
 
 #dlib에 있는 얼굴 인식 AI 모델 가져옴
 detector = dlib.get_frontal_face_detector()
 sp = dlib.shape_predictor('dlib data/shape_predictor_68_face_landmarks.dat')
 facerec = dlib.face_recognition_model_v1('dlib data/dlib_face_recognition_resnet_model_v1.dat')
-img = cv2.imread('Abdel_Madi_Shabneh_0001.jpg')
+
+#url을 통해 스트리밍되는 esp32 cam의 영상 받음
+url = "https://<ESP32_CAM_IP>/stream"
+cap = cv2.VideoCapture(url)
+#아두이노 연결
+board = pyfirmata.Arduino('COM1')
+
+#저장한 keras 형식의 딥러닝 모델 불러옴
+model = keras.models.load_model('predict_model.keras')
+
+is_open = True#눈이 열렸는가
+    
+start = 0.0
+end = 0.0
 
 #눈을 찾아 좌표를 배열로 만드는 함수, 이미지를 입력으로 받음
 def find_eyes(img):
@@ -52,5 +71,39 @@ def find_eyes(img):
     #리스트 반환
     return result
 
-a=find_eyes(img)
-print(a)
+while True:
+    #url의 영상을 읽어서 이미지로 저장
+    #성공했다면 ret에 true 저장, 실패했다면 ret에 false 저장
+    #frame에 이미지 저장
+    ret, frame = cap.read()
+    #실패하면 반복문 탈출
+    if not ret:
+        print("Failed to grab frame")#이미지 획득에 실패했습니다 라는뜻
+        break
+    
+    #find_eyes 함수를 통해 딥러닝 완료된 모델에 넣을 데이터 수집, datas에 저장
+    datas = find_eyes(frame)
+    inputs = np.array(datas)
+    #입력값을 예측 모델에 넣고 예측값 저장
+    prediction = model.predict(inputs)
+    #예측값은 0~1 사이 실수이기 때문에 반올림
+    prediction = round(prediction)
+    
+    #시간을 재서 3초 이상 눈을 감은 상태가 유지되면 아두이노에 신호를 전송해 부저를 울리게 한다
+    if is_open == True:
+        if prediction == 0:
+            start = time.time()
+    elif is_open == False:
+        end = time.time()
+    
+    is_open = bool(prediction)
+    
+    if (end-start) > 3:#3초 이상 눈 감았을때
+        board.digital[2].write(1)#부저 활성화
+        time.sleep(1)
+        board.digital[2].write(0)#부저 비활성화
+        time.sleep(1)
+    
+    #q키를 입력하면 반복문 탈출
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
